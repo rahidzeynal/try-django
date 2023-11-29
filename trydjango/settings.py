6cf382038e1385bc481bdd591555491230060c1e
Rahid Zeynalov <zeynalovrahid1@gmail.com>
2023-11-29 16:43:38 +0400

    Added new app Articles by startapp command of manage.py and created html files and redirecting between them

--- trydjango/settings.py
+++ trydjango/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -29,7 +28,8 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
-
+# python manage.py makemigrations
+# python manage.py migrate
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'articles',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,9 @@ ROOT_URLCONF = 'trydjango.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- trydjango/views.py
+++ trydjango/views.py
@@ -1,16 +1,34 @@
 """
 To render html web pages
 """
+import random
 from django.http import HttpResponse
+from django.template.loader import render_to_string
+from articles.models import Article
 
-
-HTML_STRING = """
-<h1>Hello World</h1>
-"""
-
+# Actually Django acts like MVT system
 def home_view(request):
     """
     Take in a request(Django sends request)
     Return HTML as a response(We pick to return the response)
     """
+
+    random_article_id = random.randint(1, 4)
+
+    # Get data from database
+    article_obj = Article.objects.get(id=random_article_id)
+
+    context = {
+        "article_obj": article_obj,
+        "title": article_obj.title,
+        "id": article_obj.id,
+        "content": article_obj.content
+    }
+    # Django Templates
+    HTML_STRING = render_to_string('home-view.html', context=context)
+    # HTML_STRING = """
+    # <h1>{title} (id: {id})</h1>
+    # <p>{content}</p>
+    # """.format(**context)
+
     return HttpResponse(HTML_STRING)
